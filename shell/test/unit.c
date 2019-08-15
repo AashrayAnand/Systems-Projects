40ed47f3b9fa4bdc6af5f62a6350c7999a0a732a
Aashray <aashray@uw.edu>
2019-08-15 14:20:39 -0700

    working on round 1 of shell unit tests, making sure argument parsing is 100% successful, before moving onto redirection

--- shell/test/unit.c
+++ shell/test/unit.c
@@ -7,7 +7,7 @@
 
 // test function that parses input line into array of char
 // string arguments
-void test_parse_args(void){
+void multiple_args(void){
   char basic_ls[] = "ls -l -h -a";
   char *basic_ls_args[] = {"ls", "-l", "-h", "-a", NULL};
   char **basic_ls_args_result = parse_args(basic_ls);
@@ -17,6 +17,42 @@ void test_parse_args(void){
     CU_ASSERT(strcmp(basic_ls_args[i], basic_ls_args_result[i]) == 0);
     i++;
   }
+
+  CU_ASSERT(!basic_ls_args_result[i] && !basic_ls_args[i]);
+}
+
+void blank_cmd(void){
+  char blank_command[] = "";
+  char **blank_args = parse_args(blank_command);
+  CU_ASSERT(strlen(blank_args[0]) == 0);
+}
+
+void extra_space(void){
+  char spaced_ls[] = "ls    -l    -h    -a";
+  char *spaced_ls_args[] = {"ls", "-l", "-h", "-a", NULL};
+  char **spaced_ls_args_result = parse_args(spaced_ls);
+  int i = 0;
+  // check the string is parsed into the correct arguments
+  while(spaced_ls_args[i] && spaced_ls_args_result[i]){
+    CU_ASSERT(strcmp(spaced_ls_args[i], spaced_ls_args_result[i]) == 0);
+    i++;
+  }
+
+  CU_ASSERT(!spaced_ls_args[i] && !spaced_ls_args_result[i]);
+}
+
+void trailing_space(void){
+  char trailing_cat[] = "cat shell.c    ";
+  char *trailing_cat_args[] = {"cat", "shell.c", NULL};
+  char **trailing_cat_args_results = parse_args(trailing_cat);
+  int i = 0;
+  // check the string is parsed into the correct arguments
+  while(trailing_cat_args[i] && trailing_cat_args_results[i]){
+    CU_ASSERT(strcmp(trailing_cat_args[i], trailing_cat_args_results[i]) == 0);
+    i++;
+  }
+
+  CU_ASSERT(!trailing_cat_args_results[i] && !trailing_cat_args[i]);
 }
 
 int main(){
@@ -24,7 +60,10 @@ int main(){
   CU_initialize_registry();
   CU_pSuite suite = CU_add_suite("arg_test", 0, 0);
 
-  CU_add_test(suite, "basic_args", test_parse_args);
+  CU_add_test(suite, "multiple arguments", multiple_args);
+  CU_add_test(suite, "blank command", blank_cmd);
+  CU_add_test(suite, "extra space", extra_space);
+  CU_add_test(suite, "trailing spaces", trailing_space);
 
   CU_basic_set_mode(CU_BRM_VERBOSE);
   CU_basic_run_tests();
